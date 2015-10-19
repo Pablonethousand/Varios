3c66c4865809fc6ec496256837e08fef07fec224
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2015-10-19 09:02:59 +0200

    Inicio proyectoBeta

---
+++ proyectoBeta/src/proyectobeta/ProyectoBeta.java
@@ -0,0 +1,15 @@
+
+package proyectobeta;
+
+/**
+ *
+ * @author plago-bergonpazos
+ */
+public class ProyectoBeta {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Proyect Beta");
+    }
+    
+}
