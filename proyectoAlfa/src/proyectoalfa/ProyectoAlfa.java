3421407f54943f751ec4f4d3ab1b9a1e4a0e0119
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2015-10-19 09:18:42 +0200

    Inicio proyectoAlfa

---
+++ proyectoAlfa/src/proyectoalfa/ProyectoAlfa.java
@@ -0,0 +1,15 @@
+
+package proyectoalfa;
+
+/**
+ *
+ * @author plago-bergonpazos
+ */
+public class ProyectoAlfa {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Proyect Alfa");
+    }
+    
+}
